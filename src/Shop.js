7360d6bf2fc528e20ed73dfc70573b08f1a1240d
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-09-05 15:56:23 +0530

    setting up react application for redux

---
+++ src/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+      <button className="btn btn-primary mx-2">-</button>
+      Add to your cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}

8810e60ad60567763bbe0e2f7dfb6ce948c10026
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-09-05 20:47:13 +0530

    complete redux code done

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {actionCreators } from './state/index'
 
 export default function Shop() {
+  const amount = useSelector(state=> state.amount);
+  const dispatch = useDispatch();
+  const {withdrawMoney , depositMoney } = bindActionCreators(actionCreators , dispatch)
   return (
     <div>
-      <button className="btn btn-primary mx-2">-</button>
-      Add to your cart
-      <button className="btn btn-primary mx-2">+</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+      Update Balance {amount}
+      <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
     </div>
   )
 }
